8b625c6f2ce737a8be347159dc551eb11dc9e1a3
Martingale42 <112751010@nccu.edu.tw>
2025-10-30 21:53:49 +0800

    Fix the issue so that apps successfully start when container is up.

--- assets/x11vnc-session.sh
+++ assets/x11vnc-session.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-/usr/bin/x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :0 -repeat -nopw -wait 5 -permitfiletransfer -tightfilexfer -auth /var/run/slim.auth
-#/usr/bin/supervisorctl restart slim
+# Wait for X server to be ready
+sleep 2
+/usr/bin/x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :0 -repeat -nopw -wait 5 -permitfiletransfer -tightfilexfer -forever -shared
 
 
